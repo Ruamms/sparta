ccb081db404d057a2b1941854b439def624131ba
Ruamms <ruamms3@gmail.com>
2024-04-27 13:22:23 -0300

    Foi incluido novos campos como obrigatorios no cadastro

--- usuario/adicionar.php
+++ usuario/adicionar.php
@@ -16,20 +16,20 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-    <!--  ícones do Bootstrap -->
+    <!-- ícones do Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <!--  mascaras jquery -->
+    <!-- mascaras jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <link rel="stylesheet" href="../public/style.css">
-    <title>Adicionar usuario</title>
+    <title>Adicionar usuário</title>
 </head>
 
 <body>
     <nav class="sticky-top navbar navbar-expand-md navbar-light bg-dark py-1 box-shadow">
         <div class="container">
-
-            <img class="imagem-login" src="../img/Sparta Suplementos - Logo.png" alt="sparta" />
+            <a class="navbar-brand" href="#">
+                <img class="imagem-login" src="../img/Sparta Suplementos - Logo.png" alt="sparta" />
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação">
@@ -37,33 +37,26 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
-
-
                     <li class="nav-item">
-                        <p class="text-center"><a class="nav-link text-warning" href="../index.php"><i
-                                    class="bi bi-house " data-bs-toggle="tooltip" data-bs-placement="top"
-                                    title="Inicio"></i><br>Inicio</a>
-                        </p>
+                        <a class="nav-link text-warning" href="../index.php"><i class="bi bi-house"
+                                data-bs-toggle="tooltip" data-bs-placement="top" title="Inicio"></i><br>Inicio</a>
                     </li>
-
                 </ul>
             </div>
         </div>
     </nav>
     <h2 class="mt-3 text-center">Cadastro</h2>
 
-
     <section class="container mt-3">
         <form action="salvar.php" class="container" method="post">
-
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="nome">Nome:</label>
                     <input type="text" class="form-control" id="nome" name="nome" required>
                 </div>
-                <div class="form-group  col-md-6 ">
-                    <label for="endereco">Endereço:</label>
-                    <input type="text" class="form-control" id="endereco" name="endereco" required>
+                <div class="form-group col-md-6">
+                    <label for="cpf">CPF:</label>
+                    <input type="text" class="form-control" id="cpf" name="cpf" required maxlength="14">
                 </div>
             </div>
             <div class="form-row">
@@ -71,51 +64,105 @@
                     <label for="telefone">Telefone:</label>
                     <input type="tel" class="form-control" id="telefone" name="telefone" required>
                 </div>
-                <input type="hidden" id="tipo_cliente" name="tipo_cliente" value="cliente">
                 <div class="form-group col-md-6">
                     <label for="data_nasc">Data de Nascimento:</label>
                     <input type="date" class="form-control" id="data_nasc" name="data_nasc" required>
                 </div>
             </div>
-
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="cpf">CPF:</label>
-                    <input type="text" class="form-control" id="cpf" name="cpf" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}"
-                        title="Digite o CPF no formato 999.999.999-99" required>
-                </div>
+            <div class="form-row">                
                 <div class="form-group col-md-6">
                     <label for="email">Email:</label>
-                    <input type="email" class="form-control" id="email" name="email"
-                        pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
-                        title="Por favor, insira um endereço de e-mail válido" required>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="confirmar_email">Confirmar Email:</label>
+                    <input type="confirmar_email" class="form-control" id="confirmar_email" name="confirmar_email" required>
                 </div>
             </div>
-
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="senha">Senha:</label>
                     <input type="password" class="form-control" id="senha" name="senha" required>
                 </div>
+                <div class="form-group col-md-6">
+                    <label for="confirmar_senha">Confirmar Senha:</label>
+                    <input type="password" class="form-control" id="confirmar_senha" name="confirmar_senha" required>
+                </div>
             </div>
-
-
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="cep">CEP:</label>
+                    <input type="text" class="form-control" id="cep" name="cep" required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="endereco">Endereço:</label>
+                    <input type="text" class="form-control" id="endereco" name="endereco" required readonly>
+                </div>                
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="numero">Número:</label>
+                    <input type="text" class="form-control" id="numero" name="numero" required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="cidade">Cidade:</label>
+                    <input type="text" class="form-control" id="cidade" name="cidade" required readonly>
+                </div>              
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="estado">Estado:</label>
+                    <input type="text" class="form-control" id="estado" name="estado" required readonly>
+                </div>    
+                <div class="form-group col-md-6">
+                    <label for="complemento">Complemento:</label>
+                    <input type="text" class="form-control" id="complemento" name="complemento">
+                </div>             
+            </div>
             <div class="text-center m-3">
                 <button type="submit" class="btn w-100 btn-warning ">Finalizar Cadastro</button>
-            </div>
             </div>
         </form>
     </section>
     <script>
-        $(document).ready(function () {
-            $('#cpf').mask('000.000.000-00', { reverse: true });
-        });
+    $(document).ready(function () {
+        // Máscaras para os campos
+        $('#cpf').mask('000.000.000-00', { reverse: true });
+        $('#telefone').mask('00-00000-0000', { reverse: true });
+        $('#cep').mask('00000-000');
         
-        $(document).ready(function () {
-            $('#telefone').mask('00-00000-0000', { reverse: true });
+        // Evento blur para preencher o endereço usando a API ViaCEP
+        $('#cep').blur(function(){
+            var cep = $(this).val().replace('-', '');
+            $.getJSON('https://viacep.com.br/ws/'+cep+'/json/', function(data) {
+                $('#endereco').val(data.logradouro);
+                $('#cidade').val(data.localidade);
+                $('#estado').val(data.uf);
+            });
         });
-    </script>
 
+        // Evento de envio do formulário
+        $('form').submit(function(event) {
+            // Verificação das senhas
+            var senha = $('#senha').val();
+            var confirmarSenha = $('#confirmar_senha').val();
+            if (senha !== confirmarSenha) {
+                alert('As senhas não são iguais. Por favor, corrija.');
+                event.preventDefault(); // Impede o envio do formulário
+                return;
+            }
+
+            // Verificação dos e-mails
+            var email = $('#email').val();
+            var confirmarEmail = $('#confirmar_email').val();
+            if (email !== confirmarEmail) {
+                alert('Os e-mails não são iguais. Por favor, corrija.');
+                event.preventDefault(); // Impede o envio do formulário
+                return;
+            }
+        });
+    });
+</script>
 </body>
 
 </html>
--- usuario/salvar.php
+++ usuario/salvar.php
@@ -5,17 +5,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>sparta</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="../public/style.css">
     <title>Adicionar usuario</title>
@@ -27,8 +20,7 @@
 
             <img class="imagem-login" src="../img/Sparta Suplementos - Logo.png" alt="sparta" />
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -51,7 +43,12 @@
     $cpf = $_POST['cpf'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-    
+    $cep = $_POST['cep'];
+    $numero = $_POST['numero'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+    $complemento = $_POST['complemento'];
+    $numero_cartao = isset($_POST['numero_cartao']) ? $_POST['numero_cartao'] : ''; 
 
     // Verificar se o e-mail já existe na tabela de usuários
     $verificarEmail = $conn->prepare("SELECT * FROM usuario WHERE email = ?");
@@ -62,7 +59,6 @@
     if ($resultEmail->num_rows > 0) {
         // O e-mail já existe
         echo '<div class="container text-center">';
-
         echo '<div class="alert alert-danger my-5">';
         echo '<h3 class="text-center">E-mail já cadastrado. Por favor, escolha outro e-mail.</h3>';
         echo '</div>';
@@ -78,22 +74,19 @@
         if ($resultCPF->num_rows > 0) {
             // O CPF já existe
             echo '<div class="container text-center">';
-
-        echo '<div class="alert alert-danger my-5">';
-        echo '<h3 class="text-center">CPF já cadastrado. Por favor, escolha outro CPF.</h3>';
-        echo '</div>';
-        echo '<a class="btn btn-warning  text-center" href="adicionar.php"">Voltar</a>';
-        echo '</div>';
+            echo '<div class="alert alert-danger my-5">';
+            echo '<h3 class="text-center">CPF já cadastrado. Por favor, escolha outro CPF.</h3>';
+            echo '</div>';
+            echo '<a class="btn btn-warning  text-center" href="adicionar.php"">Voltar</a>';
+            echo '</div>';
         } else {
             // Inserir dados na tabela "usuario"
-            $sqlUsuario = "INSERT INTO usuario (nome, email, senha, perfil) 
-                       VALUES (?, ?, ?, 'cliente')";
+            $sqlUsuario = "INSERT INTO usuario (nome, email, senha, perfil) VALUES (?, ?, ?, 'cliente')";
             $stmtUsuario = $conn->prepare($sqlUsuario);
 
             if ($stmtUsuario === false) {
                 die("Erro na preparação da declaração de inserção do usuário: " . $conn->error);
             }
-
 
             // Bind parameters para a declaração de inserção do usuário
             $stmtUsuario->bind_param("sss", $nome, $email, $senha);
@@ -109,8 +102,8 @@
             $stmtUsuario->close();
 
             // Inserir dados na tabela "cliente"
-            $sqlCliente = "INSERT INTO cliente (usuario_id, nome, endereco, telefone, data_nasc, cpf, email) 
-                   VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $sqlCliente = "INSERT INTO cliente (usuario_id, nome, endereco, telefone, data_nasc, cpf, email, cep, numero, cidade, estado, complemento, numero_cartao) 
+               VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             $stmtCliente = $conn->prepare($sqlCliente);
 
             if ($stmtCliente === false) {
@@ -118,8 +111,8 @@
             }
 
             // Bind parameters para a declaração de inserção do cliente
-            $stmtCliente->bind_param("issssss", $usuario_id, $nome, $endereco, $telefone, $data_nasc, $cpf, $email,);
-
+            $stmtCliente->bind_param("issssssssssss", $usuario_id, $nome, $endereco, $telefone, $data_nasc, $cpf, $email, $cep, $numero, $cidade, $estado, $complemento, $numero_cartao);
+            
             if ($stmtCliente->execute() === false) {
                 die("Erro ao executar a declaração de inserção do cliente: " . $stmtCliente->error);
             }
@@ -135,6 +128,7 @@
     // Fechar a conexão
     $conn->close();
     ?>
+
 
 </body>
 
