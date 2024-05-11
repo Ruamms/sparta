c4ddb14f1f75d6869b6c277283ba8576dc9a3ce6
Ruamms <ruamms3@gmail.com>
2024-05-11 19:37:32 -0300

    Correção index

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <!--  ícones do Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-  <link rel="stylesheet" href="../public/style.css">
+  <link rel="stylesheet" href="./public/style.css">
 
 </head>
 
@@ -27,23 +27,16 @@
   <nav class="sticky-top navbar navbar-expand-md navbar-light bg-dark py-1 box-shadow">
     <div class="container">
 
-      <img class="imagem-login" src="../img/Sparta Suplementos - Logo.png" alt="sparta" />
+      <img class="imagem-login" src="./img/Sparta Suplementos - Logo.png" alt="sparta" />
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
-          <!-- relatorio de compra-->
-          <li class="nav-item mr-5">
-            <p class="text-center"><a class="nav-link text-warning" href="../carrinho/relatorio/relatorio_compra.php"><i class="bi bi-bag-check " data-bs-toggle="tooltip" data-bs-placement="top" title="Minhas Compras"></i><br>
-                Minhas compras</a>
-            </p>
-          </li>
-
           <!-- carrinho de compra-->
           <li class="nav-item mr-5">
-            <p class="text-center"><a class="nav-link text-warning" href="../carrinho/carrinho.php">
+            <p class="text-center"><a class="nav-link text-warning" href="carrinho/carrinho.php">
                 <i class="bi bi-cart" data-bs-toggle="tooltip" data-bs-placement="top" title="Carrinho de Compras"></i><br>
                 Carrinho</a></p>
           </li>
@@ -52,11 +45,19 @@
           if (!isset($_SESSION)) {
             session_start();
           }
-          include("../usuario/conexao.php");
+          include("usuario/conexao.php");
           if (isset($_SESSION['perfil']) && $_SESSION['perfil'] === 'cliente') {
             echo '<li class="nav-item mr-5">
+                                    <p class="text-center">
+                                        <a class="nav-link text-warning" href="../carrinho/relatorio/relatorio_compra.php">
+                                        <i class="bi bi-bag-check " data-bs-toggle="tooltip" data-bs-placement="top" title="Minhas Compras"></i><br>
+                                            Minhas compras
+                                        </a>
+                                    </p>
+                                </li>';  
+            echo '<li class="nav-item mr-5">
                       <p class="text-center">
-                          <a class="nav-link text-warning" href="../usuario/perfil.php">
+                          <a class="nav-link text-warning" href="usuario/perfil.php">
                               <i class="bi bi-person-circle " data-bs-toggle="tooltip" data-bs-placement="top" title="Configuração"></i><br>
                               Perfil
                           </a>
@@ -64,7 +65,7 @@
                   </li>';
             echo '<li class="nav-item mr-5">
                       <p class="text-center">
-                      <a class="nav-link text-warning" href="../index.php">
+                      <a class="nav-link text-warning" href="index.php">
                       <i class="bi bi-box-arrow-right " data-bs-toggle="tooltip" data-bs-placement="top" title="Sair"></i><br>
                               Sair
                           </a>
@@ -89,14 +90,14 @@
   <div class="container text-center mt-3">
     <div class="row align-items-start">
       <div class="col">
-        <img class="imagem-login" src="../img/suplemento-alimentar-o-que-e-par.png" alt="sparta" />
+        <img class="imagem-login" src="img/suplemento-alimentar-o-que-e-par.png" alt="sparta" />
       </div>
       <div class="col">
         <h2 class="m-5 ">Seja Bem vindo,</h2>
-        <?php include 'id_usuario.php'; ?>
+        <?php include 'usuario/id_usuario.php'; ?>
       </div>
       <div class="col">
-        <img class="imagem-login" src="../img/suplementos.png" alt="sparta" />
+        <img class="imagem-login" src="img/suplementos.png" alt="sparta" />
       </div>
     </div>
   </div>
@@ -142,7 +143,7 @@
           echo '<h4>' . $row["nome"] . '</h4>';
           echo '<p>' . $row["descricao"] . '</p>';
 
-          echo '<a class="btn btn-warning mt-2" href="../carrinho/index.php">Ver Mais</a>';
+          echo '<a class="btn btn-warning mt-2" href="carrinho/index.php">Ver Mais</a>';
           echo '</div>';
           echo '</div>';
           echo '</div>';
@@ -179,24 +180,24 @@
 
     <div class="card col-sm-4 m-1">
       <div class="container">
-        <img class="card-img-top" src="../img/ki1.jpg" alt="Imagem de capa do card">
+        <img class="card-img-top" src="img/ki1.jpg" alt="Imagem de capa do card">
         <div class="card-body text-center">
           <h2 class="card-title">Hipertofria</h2>
           <p class="">Combo Definição</p>
           <h3 class="card-text m-3 ">R$210,00</h3>
-          <a href="../carrinho/index.php" class="btn btn-warning m-3">Ver mais</a>
+          <a href="carrinho/index.php" class="btn btn-warning m-3">Ver mais</a>
         </div>
       </div>
     </div>
 
     <div class="card col-sm-4 m-1">
       <div class="container">
-        <img class="card-img-top" src="../img/kit4.jpg" alt="Imagem de capa do card">
+        <img class="card-img-top" src="img/kit4.jpg" alt="Imagem de capa do card">
         <div class="card-body text-center  ">
           <h2 class="card-title">Power Treino</h2>
           <p class="">Melhore Seu Treino</p>
           <h3 class="card-text  ">R$190,00</h3>
-          <a href="../carrinho/index.php" class="btn btn-warning m-2">Ver mais</a>
+          <a href="carrinho/index.php" class="btn btn-warning m-2">Ver mais</a>
         </div>
       </div>
     </div>
@@ -204,12 +205,12 @@
 
     <div class="card col-sm-4 m-1 ">
       <div class="container">
-        <img class="card-img-top" src="../img/kit3.jpg" alt="Imagem de capa do card">
+        <img class="card-img-top" src="img/kit3.jpg" alt="Imagem de capa do card">
         <div class="card-body text-center ">
           <h2 class="card-title">Ganho de Massa</h2>
           <p class="">Ganhe massa muscular</p>
           <h3 class="card-text ">R$270,00</h3>
-          <a href="../carrinho/index.php" class="btn btn-warning m-3">Ver mais</a>
+          <a href="carrinho/index.php" class="btn btn-warning m-3">Ver mais</a>
         </div>
       </div>
     </div>
@@ -271,9 +272,9 @@
         <div class="mt-3">
           <h4 class="h6">Franquias</h4>
           <ul class="list-unstyled  mt-3">
-            <li><a class="text-warning" href="./filiais.php">São Paulo</a></li>
-            <li><a class="text-warning" href="./filiais.php">Minas Gerais</a></li>
-            <li><a class="text-warning" href="./filiais.php">Braslia</a></li>
+            <li><a class="text-warning" href="usuario/filiais.php">São Paulo</a></li>
+            <li><a class="text-warning" href="usuario/filiais.php">Minas Gerais</a></li>
+            <li><a class="text-warning" href="usuario/filiais.php">Braslia</a></li>
           </ul>
         </div>
 
--- usuario/filiais.php
+++ usuario/filiais.php
@@ -6,17 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>sparta</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <!-- mascaras jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
@@ -31,26 +24,20 @@
 
             <img class="imagem-login" src="../img/Sparta Suplementos - Logo.png" alt="sparta" />
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
-                    <!-- relatorio de compra-->
-                    <li class="nav-item mr-5">
-                        <p class="text-center"><a class="nav-link text-warning"
-                                href="../carrinho/relatorio/relatorio_compra.php"><i class="bi bi-bag-check "
-                                    data-bs-toggle="tooltip" data-bs-placement="top" title="Minhas Compras"></i><br>
-                                Minhas compras</a>
+                    <li class="nav-item">
+                        <p class="text-center"><a class="nav-link text-warning" href="../index.php"><i class="bi bi-house " data-bs-toggle="tooltip" data-bs-placement="top" title="Inicio"></i><br>Inicio</a>
                         </p>
-                    </li>
+                    </li>                   
 
                     <!-- carrinho de compra-->
                     <li class="nav-item mr-5">
                         <p class="text-center"><a class="nav-link text-warning" href="../carrinho/carrinho.php">
-                                <i class="bi bi-cart" data-bs-toggle="tooltip" data-bs-placement="top"
-                                    title="Carrinho de Compras"></i><br>
+                                <i class="bi bi-cart" data-bs-toggle="tooltip" data-bs-placement="top" title="Carrinho de Compras"></i><br>
                                 Carrinho</a></p>
                     </li>
                     <!--Perfil-->
@@ -60,6 +47,14 @@
                     }
                     include("../usuario/conexao.php");
                     if (isset($_SESSION['perfil']) && $_SESSION['perfil'] === 'cliente') {
+                        echo '<li class="nav-item mr-5">
+                                    <p class="text-center">
+                                        <a class="nav-link text-warning" href="../carrinho/relatorio/relatorio_compra.php">
+                                        <i class="bi bi-bag-check " data-bs-toggle="tooltip" data-bs-placement="top" title="Minhas Compras"></i><br>
+                                            Minhas compras
+                                        </a>
+                                    </p>
+                                </li>';        
                         echo '<li class="nav-item mr-5">
                                     <p class="text-center">
                                         <a class="nav-link text-warning" href="../usuario/perfil.php">
@@ -121,7 +116,7 @@
             <div class="col-md-6">
                 <img src="../img/cidades/Campo Belo MG. Oeste de Minas. F.png" alt="Cidade 2" class="img-fluid mb-3">
             </div>
-              
+
             <div class="col-md-6">
                 <h2>Minas Gerais</h2>
                 <p>Belo Horizonte, a capital de Minas Gerais, é conhecida por sua rica história, arquitetura marcante e
@@ -162,4 +157,3 @@
 </body>
 
 </html>
-
